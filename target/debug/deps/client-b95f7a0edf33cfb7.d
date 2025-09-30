6cdcefcedbe6d4df5adabc96578659feee13ec8d
LuEklund <luckeeklund@gmail.com>
2025-09-30 22:34:01 +0300

    new server module

--- target/debug/deps/client-b95f7a0edf33cfb7.d
+++ target/debug/deps/client-b95f7a0edf33cfb7.d
@@ -1,12 +1,5 @@
-/home/lucas/projects/Youtube/PlanetaryZigma/target/debug/deps/client-b95f7a0edf33cfb7.d: spacetime/client/src/main.rs spacetime/client/src/module_bindings/mod.rs spacetime/client/src/module_bindings/add_reducer.rs spacetime/client/src/module_bindings/identity_connected_reducer.rs spacetime/client/src/module_bindings/identity_disconnected_reducer.rs spacetime/client/src/module_bindings/person_table.rs spacetime/client/src/module_bindings/person_type.rs spacetime/client/src/module_bindings/say_hello_reducer.rs
+/home/lucas/projects/Youtube/PlanetaryZigma/target/debug/deps/client-b95f7a0edf33cfb7.d: spacetime/client/src/main.rs
 
-/home/lucas/projects/Youtube/PlanetaryZigma/target/debug/deps/libclient-b95f7a0edf33cfb7.rmeta: spacetime/client/src/main.rs spacetime/client/src/module_bindings/mod.rs spacetime/client/src/module_bindings/add_reducer.rs spacetime/client/src/module_bindings/identity_connected_reducer.rs spacetime/client/src/module_bindings/identity_disconnected_reducer.rs spacetime/client/src/module_bindings/person_table.rs spacetime/client/src/module_bindings/person_type.rs spacetime/client/src/module_bindings/say_hello_reducer.rs
+/home/lucas/projects/Youtube/PlanetaryZigma/target/debug/deps/libclient-b95f7a0edf33cfb7.rmeta: spacetime/client/src/main.rs
 
 spacetime/client/src/main.rs:
-spacetime/client/src/module_bindings/mod.rs:
-spacetime/client/src/module_bindings/add_reducer.rs:
-spacetime/client/src/module_bindings/identity_connected_reducer.rs:
-spacetime/client/src/module_bindings/identity_disconnected_reducer.rs:
-spacetime/client/src/module_bindings/person_table.rs:
-spacetime/client/src/module_bindings/person_type.rs:
-spacetime/client/src/module_bindings/say_hello_reducer.rs:
