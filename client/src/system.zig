1d1bd082c36721a421e5dd3882c7f0dfc901e5b1
LuEklund <luckeeklund@gmail.com>
2026-07-17 00:46:57 +0300

    moced things again

--- client/src/system.zig
+++ client/src/system.zig
@@ -6,6 +6,7 @@ const yes = @import("yes");
 const NetworkManager = @import("system/NetworkManager.zig");
 const AssetServer = @import("shared").AssetServer;
 const Animation = @import("system/Animations.zig");
+const motion = @import("system/motion.zig");
 pub const Renderer = @import("Renderer.zig");
 
 pub const Camera = @import("system/Camera.zig");
@@ -105,25 +106,7 @@ pub const Context = struct {
         try self.animation.update(info, &self.renderer.inner.skeletons);
 
         const server_time = self.network_manager.server_tick_estimate * shared.tick_seconds;
-        for (info.world.entities.values()) |*entity| {
-            const motion = entity.update_motion orelse continue;
-            const motion_time = @as(f32, @floatFromInt(motion.tick)) * shared.tick_seconds;
-            const age = server_time - motion_time;
-            const target = motion.position + nz.vec.scale(motion.velocity, age);
-
-            if (motion.tick != entity.smoothed_moiton_tick) {
-                entity.position_error = entity.transform.position - target;
-                entity.smoothed_moiton_tick = motion.tick;
-            }
-
-            const error_decay = std.math.pow(f32, 1e-5, info.delta_time);
-            entity.position_error = nz.vec.scale(entity.position_error, error_decay);
-            entity.transform.position = target + entity.position_error;
-
-            const target_rotation = nz.Quat(f32).fromVec(motion.rotation);
-            const rotation_decay = std.math.pow(f32, 1e-5, info.delta_time);
-            entity.transform.rotation = entity.transform.rotation.slerp(target_rotation, 1.0 - rotation_decay);
-        }
+        motion.evaluate(info, server_time);
 
         if (!paused_for_input and !info.world.pause_menu_open and !info.world.options_menu_open) info.world.camera.update(info);
         info.world.controller.resetMouseDelta();
